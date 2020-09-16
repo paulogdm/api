327a302ae0cc9533c9a490b8d3e5bc13a25eccaf
CJ Lazell <cjlazell@gmail.com>
2020-09-15 22:19:51 -0500

    defer scripts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,5 @@
-import AppNavigation, {Screen} from '~/navigations/AppNavigation'
 import HomeScreen from '~/screens/Home'
 
 export default function IndexPage() {
-  return (
-    <AppNavigation>
-      <Screen name="Home" component={HomeScreen} />
-    </AppNavigation>
-  )
+  return <HomeScreen />
 }
